bb8f191ae97a3dacea53297b9b46973c5963212e
yugeshnukala <yugeshnukala95@gmail.com>
2020-08-01 16:06:45 +0530

    Updates

---
+++ precache-manifest.8ad3dd8d81d1c2f297c81de11f7e9386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e474ef17817c971262c9cde01eccba08",
+    "url": "/Tictactoe-using-React/index.html"
+  },
+  {
+    "revision": "0890189714a5eac179f4",
+    "url": "/Tictactoe-using-React/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1cd4cef6528a83e98721",
+    "url": "/Tictactoe-using-React/static/css/main.e8b8a7e3.chunk.css"
+  },
+  {
+    "revision": "0890189714a5eac179f4",
+    "url": "/Tictactoe-using-React/static/js/2.96a7d7d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tictactoe-using-React/static/js/2.96a7d7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd4cef6528a83e98721",
+    "url": "/Tictactoe-using-React/static/js/main.2080581a.chunk.js"
+  },
+  {
+    "revision": "71c3a4f5f4d55a7fcdb7",
+    "url": "/Tictactoe-using-React/static/js/runtime-main.b84fd4b1.js"
+  }
+]);
